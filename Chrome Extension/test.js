076afb2475d6511c795ae8c4eb6181841b3d241a
Shrub8 <stevenwalter000@gmail.com>
2022-10-04 03:52:23 -0500

    links work!

--- Chrome Extension/test.js
+++ Chrome Extension/test.js
@@ -1,12 +1,31 @@
-const text = document.getElementsByTagName('pre')
+const pre = document.getElementsByTagName('pre');
 
-let lName = 'Singh'
-let fName = 'Samriddhi'
-
-for (let i=0; i < text.length; i++)
+for (const block of pre)
 {
-    if(text[i].innerHTML.includes(lName+' '+fName)) 
+    let lines = block.firstChild.innerText.split(/\r?\n|\r|\n/g) //crazy looking shit just means '\n or \r' in regex
+    let profs = []
+    let blacklist = ['STAFF', 'Instructor', '-------------------------------------------------------']
+    
+    for (const line of lines)
     {
-        text[i].innerHTML = text[i].innerHTML.replaceAll(lName+' '+fName, '<a href="https://www.ratemyprofessors.com/search/teachers?query='+fName+'%20+'+lName+'&sid=U2Nob29sLTEwNjA=" target="_blank">'+lName+' '+fName+'</a>');
+        let valid = true
+        const prof = line.substring(130).trim() // Should be the entire professor's name with whitespace trimmed
+        blacklist.forEach(a => (valid = valid && !a.includes(prof))) // magic
+        if (valid && profs.indexOf(prof) === -1)
+            profs.push(prof)
     }
+
+    for (const prof of profs)
+    {
+        if (block.firstChild.innerText.includes(prof)) 
+        {
+            let linkProf = prof.split(' ').join('%20+') // replaces spaces with link version of a space
+            block.firstChild.innerHTML = block.firstChild.innerHTML.replaceAll(prof, '<a href="https://www.ratemyprofessors.com/search/teachers?query='+linkProf+'&sid=U2Nob29sLTEwNjA=" target="_blank">'+prof+'</a>')
+        }
+    }
+
+    // if(text[i].innerHTML.includes(lName+' '+fName)) 
+    // {
+    //     text[i].innerHTML = text[i].innerHTML.replaceAll(lName+' '+fName, '<a href="https://www.ratemyprofessors.com/search/teachers?query='+fName+'%20+'+lName+'&sid=U2Nob29sLTEwNjA=" target="_blank">'+lName+' '+fName+'</a>');
+    // }
 }
