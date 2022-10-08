2cfdb82d729de31df9fb454f9a9e687e751d1c3b
Shrub8 <stevenwalter000@gmail.com>
2022-10-08 14:31:17 -0500

    added bl+wl, changed ui for menu

--- Chrome Extension/popup.js
+++ Chrome Extension/popup.js
@@ -1,69 +1,101 @@
-const listElements = document.getElementsByTagName('li')
-var blacklistArray = []
-chrome.storage.sync.get('blacklistStorage', function(data){
-  blacklistArray = data.blacklistStorage
-  repopulate()
+var lists = [[], []] // 0 - blacklist, 1 - whitelist
+var listNames = ['blacklist', 'whitelist']
+
+// chrome.storage.sync.set({'listStor': [[],[]]})
+
+// Repopulate from stored data
+chrome.storage.sync.get({'listStor': [[], []]}, function(data) {
+  lists = data.listStor
+  if (lists[0].length > 0)
+    repopulateFrom(0) // bl
+  if (lists[1].length > 0)
+    repopulateFrom(1) // wl
 })
 
-document.getElementById('addBtn').addEventListener('click', newElement)
-document.getElementById('blInput').addEventListener('keypress', function(event) {
+// Top menu
+var blTab = document.getElementById('blTab')
+var wlTab = document.getElementById('wlTab')
+
+blTab.addEventListener('click', function(event) {
+  wlTab.className = wlTab.className.replaceAll(' active', '')
+  blTab.className += ' active'
+})
+
+wlTab.addEventListener('click', function(event) {
+  blTab.className = blTab.className.replaceAll( ' active', '')
+  wlTab.className += ' active'
+})
+
+// Add button functionality
+document.getElementById('addBtn').addEventListener('click', function (event) {
+   let num = (blTab.className.includes('active')) ? 0 : 1
+   newElement(num)
+})
+
+// Enter key functionality
+document.getElementById('input').addEventListener('keypress', function(event) {
   if (event.key === "Enter") {
     event.preventDefault()
-    newElement()
+    let num = (blTab.className.includes('active')) ? 0 : 1
+    newElement(num)
   }
 })
 
-function repopulate() {
-  for(const repo of blacklistArray){
-    console.log('1')
-    let li = document.createElement('li')
-    let inVal = repo
-    let t = document.createTextNode(inVal)
-    li.appendChild(t)
-    document.getElementById('blacklist').appendChild(li)
-    addClose(li)
-  }
+function repopulateFrom(num) {
+  for (const name of lists[num])
+    repopElement(name, num)
 }
 
-// Create a new list item when clicking on the 'Add' button
-function newElement() {
+function repopElement(name, num) {
   let li = document.createElement('li')
-  let inVal = document.getElementById('blInput').value
+  let t = document.createTextNode(name)
+  li.appendChild(t)
+  addClose(li, num)
+  document.getElementById(listNames[num]).appendChild(li)
+}
+
+// Create a new list item
+function newElement(num) {
+
+  // Create list item
+  let li = document.createElement('li')
+  let inVal = document.getElementById('input').value
   let t = document.createTextNode(inVal)
   li.appendChild(t)
 
+  // Check if item is a duplicate
   let isDuplicate = false
-  for (const el of listElements) {
-    if (el.innerText.toLowerCase().includes(inVal.toLowerCase())) {
-      isDuplicate = true
-      break
+  let ul = document.getElementById(listNames[num]).children
+  if (ul !== null) {
+    for (const el of ul) {
+      if (el.innerText.toLowerCase().includes(inVal.toLowerCase())) {
+        isDuplicate = true
+        break
+      }
     }
   }
-
-  if (inVal !== '' && !isDuplicate)
-  {
-    document.getElementById('blacklist').appendChild(li)
-    blacklistArray.push(inVal)
-    chrome.storage.sync.set({'blacklistStorage': blacklistArray})
+  // Add item to list
+  if (inVal !== '' && !isDuplicate) {
+    document.getElementById(listNames[num]).appendChild(li)
+    lists[num].push(inVal)
+    chrome.storage.sync.set({'listStor': lists})
   }
 
-  chrome.storage.sync.get(['blacklistStorage'], function(items){
-    console.log(items)
-  })
+  // Clear input field
+  document.getElementById('input').value = ''
 
-  document.getElementById('blInput').value = ''
-
-  addClose(li)
+  addClose(li, num)
 }
 
-function addClose(element) {
+function addClose(element, num) {
   let xButt = document.createElement('SPAN')
   let txt = document.createTextNode('\u00D7')
   xButt.className = 'close'
   xButt.appendChild(txt)
   xButt.onclick = function() {
-    blacklistArray.splice(blacklistArray.indexOf(element.innerText.slice(0, -1).trim()), 1)
-    chrome.storage.sync.set({'blacklistStorage': blacklistArray})
+    // blacklistArray.splice(blacklistArray.indexOf(element.innerText.slice(0, -1).trim()), 1)
+    lists[num].splice(lists[num].indexOf(element.nodeValue), 1)
+    chrome.storage.sync.set({'listStor': lists})
     element.remove()
   }
   element.appendChild(xButt)
