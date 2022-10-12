02edcb71aa097fee7b45f8067aee4092beb0e076
Tristan <tdouglaskennedy@gmail.com>
2022-10-11 20:23:41 -0500

    UAHPageEdit.js comments

--- Chrome Extension/UAHPageEdit.js
+++ Chrome Extension/UAHPageEdit.js
@@ -1,33 +1,33 @@
 const pre = document.getElementsByTagName('pre');
-chrome.storage.sync.get({'listStor': [[], []]}, function(data) {
+chrome.storage.sync.get({'listStor': [[], []]}, function(data) { //Gets the data that is stored in the blacklist/whitelist
     lists = data.listStor
-    highlightAttacher()
+    highlightAttacher() //Calls two functions after the data is retrieved
     hyperlinkAttacher()
   })
 
-function hyperlinkAttacher(){
+function hyperlinkAttacher(){ //Function which replaces the professor names on the UAH page with hyperlinks
     for (const block of pre)
     {
-        let lines = block.firstChild.innerText.split(/\r?\n|\r|\n/g) //crazy looking shit just means '\n or \r' in regex
+        let lines = block.firstChild.innerText.split(/\r?\n|\r|\n/g)
         let profs = []
         let excludedNames = ['STAFF', 'Instructor', '-------------------------------------------------------']
     
         for (const line of lines){
             let valid = true
-            const prof = line.substring(130).trim() // Should be the entire professor's name with whitespace trimmed
-            excludedNames.forEach(a => (valid = valid && !a.includes(prof))) // magic
+            const prof = line.substring(130).trim() 
+            excludedNames.forEach(a => (valid = valid && !a.includes(prof))) //Checking for duplicates in the prof list
             if (valid && profs.indexOf(prof) === -1)
             profs.push(prof)
         }
 
         for (const prof of profs){ 
-            let linkProf = prof.split(' ').join('%20+') // replaces spaces with link version of a space
+            let linkProf = prof.split(' ').join('%20+') //Creating RMP link
             block.firstChild.innerHTML = block.firstChild.innerHTML.replaceAll(prof, '<a href="https://www.ratemyprofessors.com/search/teachers?query='+linkProf+'&sid=U2Nob29sLTEwNjA=" target="_blank">'+prof+'</a>')
         }
     }
 }
 
-function highlightAttacher() {
+function highlightAttacher() { //Function which replaces the line text with highlighted line text. Color is dependent upon blacklist/whitelist.
     for (const block of pre) {
         let lines = block.firstChild.innerText.split(/\r?\n|\r|\n/g)
         for (const line of lines) {
